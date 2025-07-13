2dfe66bdc31ac4b7d750ddcb242d0441c5878236
Scorpi <scorpifr@gmail.com>
2025-07-13 02:53:41 +0200

    Amelioration visuel dans menu.

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,33 +1,82 @@
 export default function Item({ item, imageDirectory }) {
-  const desc = !item.description.length ? "" : <p>{item.description}</p>;
-  const miniatureUrl = item.miniature ? item.miniature : item.image;
-  const image = !item.image ? (
-    ""
-  ) : (
-    <div className="image">
-      <a
-        href={`${imageDirectory}${item.image}`}
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img
-          className="itemPreview"
-          src={`${imageDirectory}${miniatureUrl}`}
-          alt="Item"
-        />
-      </a>
-    </div>
-  );
+  console.log(item);
+  if (item.image) {
+    return item.image_mode === "vertical" ? (
+      <ItemVertical item={item} imageDirectory={imageDirectory} />
+    ) : (
+      <ItemHorizontal item={item} imageDirectory={imageDirectory} />
+    );
+  }
 
+  return <ItemNoimage item={item} />;
+}
+
+function ItemNoimage({ item }) {
   return (
     <div className="cadre">
-      {image}
+      <div className="texte">
+        <p className="itemTitle">{item.name}</p>
+        {item.description.length && (
+          <p className="itemDescription">{item.description}</p>
+        )}
+        <p className="itemPrice">{item.price} €</p>
+      </div>
+    </div>
+  );
+}
+
+function ItemVertical({ item, imageDirectory }) {
+  const miniatureUrl = item.miniature ? item.miniature : item.image;
+  return (
+    <div className="cadre">
+      <div className="image">
+        <a
+          href={`${imageDirectory}${item.image}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="itemPreview"
+            src={`${imageDirectory}${miniatureUrl}`}
+            alt="Item"
+          />
+        </a>
+      </div>
 
       <div className="texte">
-        <p className="itemTitle">
-          {item.name} - {item.price} €
-        </p>
-        {desc}
+        <p className="itemTitle">{item.name}</p>
+        {item.description.length && (
+          <p className="itemDescription">{item.description}</p>
+        )}
+        <p className="itemPrice">{item.price} €</p>
+      </div>
+    </div>
+  );
+}
+
+function ItemHorizontal({ item, imageDirectory }) {
+  const miniatureUrl = item.miniature ? item.miniature : item.image;
+  return (
+    <div className="cadre">
+      <div className="texte">
+        <div>
+          <a
+            href={`${imageDirectory}${item.image}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="itemPreviewHorizontal"
+              src={`${imageDirectory}${miniatureUrl}`}
+              alt="Item"
+            />
+          </a>
+        </div>
+        <p className="itemTitle">{item.name}</p>
+        {item.description.length && (
+          <p className="itemDescription">{item.description}</p>
+        )}
+        <p className="itemPrice">{item.price} €</p>
       </div>
     </div>
   );
